e560befb254f81de2a120cf4aae374c2b1655f07
tevtonez <tevtonez@gmail.com>
2017-06-20 00:47:41 +0200

    add user uniqueness to user signup functionality

--- accounts/views.py
+++ accounts/views.py
@@ -8,20 +8,35 @@ def signup(request):
   message = None
 
   if request.method == "POST":
+
     if request.POST['password1'] == request.POST['password2']:
+
       try:
-        User.objects.create_user(request.POST['username'], password = request.POST['password1'])
-      except IntegrityError:
-        status = 'user already exists'
-      else:
-        message = "Congratulations, you've just signed up!"
+        User.objects.get(username = request.POST['username'])
+        message = "User already exists!"
         return render(request, 'accounts/signup.html', {
-        'message' : message,
-      })
+          'message' : message,})
+
+      except User.DoesNotExist:
+
+        try:
+          User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+        except IntegrityError:
+          message = "User hasn't been saved!"
+          return render(request, 'accounts/signup.html', {
+          'message' : message,})
+
+        else:
+          message = "Congratulations, you've just signed up!"
+          return render(request, 'accounts/signup.html', {
+          'message' : message,
+        })
+
     else:
       message = "Passwords didn't match!"
       return render(request, 'accounts/signup.html', {
         'message' : message,
       })
+
   else:
     return render(request, 'accounts/signup.html')
