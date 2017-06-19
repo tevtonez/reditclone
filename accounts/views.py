8e861e3a664ae666963889cf6ec49c0e80f9adec
tevtonez <tevtonez@gmail.com>
2017-06-20 00:52:38 +0200

    add user authentication right after signing up

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -12,7 +13,7 @@ def signup(request):
     if request.POST['password1'] == request.POST['password2']:
 
       try:
-        User.objects.get(username = request.POST['username'])
+        user = User.objects.get(username = request.POST['username'])
         message = "User already exists!"
         return render(request, 'accounts/signup.html', {
           'message' : message,})
@@ -20,7 +21,9 @@ def signup(request):
       except User.DoesNotExist:
 
         try:
-          User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+          user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+          login(request, user)
+
         except IntegrityError:
           message = "User hasn't been saved!"
           return render(request, 'accounts/signup.html', {
