e1714d59c20a3d29d8a9017357d98c69b87e3890
tevtonez <tevtonez@gmail.com>
2017-06-20 00:40:13 +0200

    add better communication to the user on signup page

--- accounts/views.py
+++ accounts/views.py
@@ -5,13 +5,23 @@ from django.contrib.auth.models import User
 
 def signup(request):
 
+  message = None
+
   if request.method == "POST":
     if request.POST['password1'] == request.POST['password2']:
-      User.objects.create_user(request.POST['username'], password = request.POST['password1'])
-      return render(request, 'accounts/signup.html')
+      try:
+        User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+      except IntegrityError:
+        status = 'user already exists'
+      else:
+        message = "Congratulations, you've just signed up!"
+        return render(request, 'accounts/signup.html', {
+        'message' : message,
+      })
     else:
+      message = "Passwords didn't match!"
       return render(request, 'accounts/signup.html', {
-        'error' : "Passwords didn't match",
+        'message' : message,
       })
   else:
     return render(request, 'accounts/signup.html')
