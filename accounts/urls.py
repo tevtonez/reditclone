1f881c73365da24e3f8634ff23a2733a03a61d3b
tevtonez <tevtonez@gmail.com>
2017-06-21 00:16:10 +0200

    move url to accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views as accoutns_views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    url(r'^signup/', accoutns_views.signup, name = 'signup'),
+    url(r'^login/', accoutns_views.usrlogin, name = 'login'),
+]
--- reditclone03/urls.py
+++ reditclone03/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from accounts import views as accoutns_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^signup/', accoutns_views.signup, name = 'signup'),
-    url(r'^login/', accoutns_views.usrlogin, name = 'login'),
+    url(r'^accounts/', include('accounts.urls')),
 ]
