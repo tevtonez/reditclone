68a7ad3c09465db5ef8c6dcb4d1b222ad192baeb
tevtonez <tevtonez@gmail.com>
2017-06-22 23:16:47 +0200

    add Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+  title = models.CharField(max_length = 200)
+  url = models.TextField()
+  pub_date = models.DateTimeField()
+  author = models.ForeignKey(User)
+  rating = models.IntegerField(default = 1)
