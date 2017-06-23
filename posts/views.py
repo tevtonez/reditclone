feb6840f5a2a090e3c2538c2fee6a7a63b4a49fd
tevtonez <tevtonez@gmail.com>
2017-06-24 00:04:53 +0200

    add home page first version

--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 
-from . import models
+from .models import Post
 
 @login_required
 def create(request):
 
   if request.method == 'POST':
     if request.POST['title'] and request.POST['url']:
-      post = models.Post()
+      post = Post()
       post.title = request.POST['title']
       post.url = request.POST['url']
       post.pub_date = timezone.datetime.now()
       post.author = request.user
       post.save()
-      return render(request, 'posts/home.html')
+      return redirect('home')
     else:
       message = "You have to provide both, a 'title' and a 'url'!"
       return render(request, 'posts/create_posts.html', {'message' : message,})
@@ -24,4 +24,8 @@ def create(request):
 
 
 def home(request):
-  return render(request, 'posts/home.html')
+  posts = Post.objects.order_by('rating')
+  return render(request, 'posts/home.html',
+    {
+    'posts':posts,
+    })
